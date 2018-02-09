e68bb9a0c8e0fd39a34dd265cc83399656bc3111
JonnyMoli95 <molinacjony@gmail.com>
2018-02-09 01:48:54 -0800

    basic UI for login/splash screen

---
+++ TagRides2/app/src/main/java/com/example/jonny/tagrides/splashscreenactivity.java
@@ -0,0 +1,17 @@
+package com.example.jonny.tagrides;
+
+import android.content.Intent;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+//this creates the activity to show our splash_screen
+public class splashscreenactivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent intent = new Intent(this, Login.class);
+        startActivity(intent);
+        finish();
+    }
+}
