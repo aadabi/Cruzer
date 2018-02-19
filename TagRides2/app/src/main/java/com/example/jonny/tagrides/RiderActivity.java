45156af6ace6c1f31ec2ada760230852c3f6166a
Harland Duman <dumanjacob@gmail.com>
2018-02-19 13:10:32 -0800

    Clicking the send button triggers an empty sendRideRequest function

--- TagRides2/app/src/main/java/com/example/jonny/tagrides/RiderActivity.java
+++ TagRides2/app/src/main/java/com/example/jonny/tagrides/RiderActivity.java
@@ -3,6 +3,7 @@ package com.example.jonny.tagrides;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class RiderActivity extends Activity {
 
@@ -10,5 +11,10 @@ public class RiderActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rider);
+    }
+
+    /** Called when the user presses the send button */
+    public void sendRideRequest(View view) {
+        
     }
 }
